3f9e24f05a7b3d7904b9d69b2c79aa39a1b18fed
Maciej Borsz <maciejborsz@gmail.com>
2014-02-19 18:53:15 +0100

    Update __init__.py
    South fix.

--- stdimage/__init__.py
+++ stdimage/__init__.py
@@ -4,16 +4,6 @@ from .fields import StdImageField
 
 try:
     from south.modelsinspector import add_introspection_rules
-    rules = [
-      (
-        (StdImageField,),
-        [],
-        {
-            "size": ["size", {"default": None}],
-            "thumbnail_size": ["thumbnail_size", {"default": None}],
-        },
-      )
-    ]
-    add_introspection_rules(rules, ["^stdimage\.fields"])
+    add_introspection_rules([], ["^stdimage\.fields"])
 except ImportError:
     pass
